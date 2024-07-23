5fda9ccd2389d7e12ac49f6f125df3dd59632bac
JosephDuvvani <josephduvvani@gmail.com>
2024-07-23 15:34:43 +0200

    Display each book as a card

--- scripts.js
+++ scripts.js
@@ -6,13 +6,58 @@ function Book(title, author, pages, haveReadIt) {
     this.pages = pages;
     this.haveRead = haveReadIt;
     this.info = function() {
-        let readStatus;
-        haveReadIt === 'yes' ? readStatus = 'already read' : readStatus = 'not read yet';
-        return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+        return `${title} by ${author}, ${pages} pages, ${this.readStatus()}`;
     };
+
+    this.readStatus = function () {
+        let status = '';
+        haveReadIt === 'yes' ? status = 'already read' : status = 'not read yet';
+        return status;
+    }
 }
 
 function addBookToLibrary(title, author, pages, haveReadIt) {
     let newBook = new Book(title, author, pages, haveReadIt);
     myLibrary.push(newBook);
+}
+
+const libraryDisplay = document.querySelector('.books');
+
+function displayLibrary() {
+   for(let book of myLibrary){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    libraryDisplay.appendChild(card);
+
+    let bookTitle = document.createElement('h1');
+    bookTitle.classList.add('book-title');
+    bookTitle.textContent = book.title;
+    card.appendChild(bookTitle);
+
+    let authorLabel = document.createElement('div');
+    authorLabel.classList.add('info-label');
+    authorLabel.textContent = 'Author:'
+    card.appendChild(authorLabel);
+
+    let authorName = document.createElement('a');
+    authorName.classList.add('author');
+    authorName.setAttribute('href', '#');
+    authorName.textContent = book.author;
+    card.appendChild(authorName);
+
+    let pagesLabel = document.createElement('div');
+    pagesLabel.classList.add('info-label');
+    pagesLabel.textContent = 'Pages:'
+    card.appendChild(pagesLabel);
+
+    let pageNum = document.createElement('div');
+    pageNum.classList.add('pages');
+    pageNum.textContent = book.pages;
+    card.appendChild(pageNum);
+
+    let readStatus = document.createElement('div');
+    readStatus.classList.add('read-status');
+    readStatus.textContent = book.readStatus();
+    card.appendChild(readStatus);
+   }
 }
